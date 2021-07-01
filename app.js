e8d95b3a8d8a26bb476a80f88cdcee48be6f73fc
Gustaf Delport <gustafdelport@gmail.com>
2021-07-01 09:34:25 +0200

    woah

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var bodyParser = require("body-parser");
-const express = require('express'); //express framework to have a higher level of methods
-const app = express(); //assign app variable the express class/method
+const express = require('express');
+
+const app = express();
 
 var http = require('http');
 var path = require("path");
@@ -8,6 +9,7 @@ var path = require("path");
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
 app.use(bodyParser.json());
 
 const server = http.createServer(app); //create a server
@@ -15,36 +17,34 @@ const server = http.createServer(app); //create a server
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     console.log('addr: ' + add);
 })
-/**********************websocket setup**************************************************************************************/
-//var expressWs = require('express-ws')(app,server);
+
+//websocket setup
 
 const WebSocket = require('ws');
 const s = new WebSocket.Server({
     server
 });
 
-//when browser sends get request, send html file to browser
-// viewed at http://localhost:3000
+//viewed at http://localhost:3000
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-//app.ws('/echo', function(ws, req) {
 s.on('connection', function (ws, req) {
 
-    /******* when server receives messsage from client trigger function with argument message *****/
+/******* when server receives messsage from client trigger function with argument message *****/
 
     ws.on('message', function (message) {
-        console.log("Received: " + message);
-        s.clients.forEach(function (client) { //broadcast incoming message to all clients (s.clients)
 
-            if (client != ws && client.readyState) { //except to the same client (ws) that sent this message
+        console.log("Received: " + message);
+        s.clients.forEach(function (client) {
+
+            if (client != ws && client.readyState) {
                 client.send("broadcast: " + message);
             }
-        });
 
-        // ws.send("From Server only to sender: "+ message); //send to client where message is from
+        });
     });
 
     ws.on('close', function () {
